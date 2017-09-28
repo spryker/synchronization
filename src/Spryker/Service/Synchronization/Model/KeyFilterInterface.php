15a08034ab85e4968d9aa57542fb775b49180fae
Ehsan Zanjani <ehsan.zanjani@spryker.com>
2017-09-28 10:51:56 +0000

    CORE-1073 Fix PR issues

--- src/Spryker/Service/Synchronization/Model/KeyFilterInterface.php
+++ src/Spryker/Service/Synchronization/Model/KeyFilterInterface.php
@@ -5,9 +5,9 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Service\Synchronization\Dependency\Service;
+namespace Spryker\Service\Synchronization\Model;
 
-interface SynchronizationToUtilSynchronizationServiceInterface
+interface KeyFilterInterface
 {
 
     /**
